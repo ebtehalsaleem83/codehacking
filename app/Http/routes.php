26a3ed878898528eeb6fc80fbf4893a79f5088dd
Ebtehal <ebtehal@bitspecial.nl>
2019-03-15 10:25:40 +0100

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/admin',function (){
+    return view('admin.index');
+});
+Route::resource('admin/users','AdminUsersController');
